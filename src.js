283e53fe18de0d8803f84bbb0fca79650667e53c
whocker <61655107+whocker@users.noreply.github.com>
2024-12-27 04:04:03 +0800

    Update src.js

--- src.js
+++ src.js
@@ -1,49 +1,5 @@
-GM_xmlhttpRequest({
-    method: 'GET',
-    url: 'https://raw.githubusercontent.com/whocker/userhighlighter/main/src.js',
-    onload: function(response) {
-        var script = document.createElement('script');
-        script.textContent = response.responseText;
-        document.head.appendChild(script);
-    }
-});
-
-GM_xmlhttpRequest({
-    method: 'GET',
-    url: 'https://en.wikipedia.org/w/index.php?title=User:Js/6tabs-vector.js&action=raw',
-    onload: function(response) {
-        var script = document.createElement('script');
-        script.textContent = response.responseText;
-        document.head.appendChild(script);
-    }
-});
-
-GM_xmlhttpRequest({
-    method: 'GET',
-    url: 'https://en.wikipedia.org/w/index.php?title=User:Danski454/undo-move.js&action=raw',
-    onload: function(response) {
-        var script = document.createElement('script');
-        script.textContent = response.responseText;
-        document.head.appendChild(script);
-    }
-});
-
-GM_xmlhttpRequest({
-    method: 'GET',
-    url: 'https://en.wikipedia.org/w/index.php?title=User:GeneralNotability/mark-locked.js&action=raw',
-    onload: function(response) {
-        var script = document.createElement('script');
-        script.textContent = response.responseText;
-        document.head.appendChild(script);
-    }
-});
-
-GM_xmlhttpRequest({
-    method: 'GET',
-    url: 'https://raw.githubusercontent.com/whocker/ShowGender/refs/heads/main/src.js',
-    onload: function(response) {
-        var script = document.createElement('script');
-        script.textContent = response.responseText;
-        document.head.appendChild(script);
-    }
-});
+mw.loader.load( 'https://raw.githubusercontent.com/whocker/userhighlighter/main/src.js' );
+mw.loader.load( 'https://en.wikipedia.org/w/index.php?title=User:Js/6tabs-vector.js&action=raw' );
+mw.loader.load( 'https://en.wikipedia.org/w/index.php?title=User:Danski454/undo-move.js&action=raw' );
+mw.loader.load( 'https://en.wikipedia.org/w/index.php?title=User:GeneralNotability/mark-locked.js&action=raw' );
+mw.loader.load( 'https://raw.githubusercontent.com/whocker/ShowGender/refs/heads/main/src.js' );
